3f64a18d8280e3cd892a48e60a0850a63a1e32e8
SchroederSimon <schroeder.ssimon@gmail.com>
2023-03-01 14:50:33 -0300

    styles changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import About from './pages/About'
 
 //Components
 import Home from './pages/Home'
+import Result from './pages/Result'
 
 function App() {
   return (
     <>
       <Home></Home>
       <About></About>
+      <Result></Result>
     </>
   )
 }
---
+++ src/pages/Result.tsx
@@ -0,0 +1,55 @@
+import '../pages/Result.css'
+
+function Result() {
+    return (
+        <div className="result-container">
+            <div className="cards-container">
+                <div className="card">
+                    <h1>Sentiment</h1>
+                    <div className="thermometer">
+                        <span className="temperature">0%</span>
+                    </div>
+                    <p></p>
+                </div>
+                <div className="card">
+                    <h1>Last sentiments</h1>
+                    <div className="card-information">
+                        <div className="square-card"></div>
+                        <p>January 20%</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <h1>Next update</h1>
+                    <div className="card-information">
+                        <p>The next update will be in X time</p>
+                    </div>
+                </div>
+            </div>
+            <div className="tweets-newspapers">
+                <div className="tweets-newspapers-grid">
+                    <div className="tweets-newspaper-info">
+                        <img src="/vite.svg" alt="" />
+                        <div className="tweets-newspaper-description">
+                            <h1>Juan Carlos</h1>
+                            <p>No se unan a inter-gold parece ser una estafa piramidal
+                                estan involucrados personajes que ya estuvieron en otra
+                                llamada Generacion Zoe
+                            </p>
+                        </div>
+                    </div>
+                    <div className="tweets-newspaper-info">
+                        <p>El tuit del que sea</p>
+                    </div>
+                    <div className="tweets-newspaper-info">
+                        <p>Noticia</p>
+                    </div>
+                    <div className="tweets-newspaper-info">
+                        <p>Noticia</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Result;
